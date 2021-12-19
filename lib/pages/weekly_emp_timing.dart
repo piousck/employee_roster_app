a6976e71d420b8647a37676e2cf638d27d2c8174
piousck <pious.james007@gmail.com>
2021-12-19 19:40:46 +0000

    commit

--- lib/pages/weekly_emp_timing.dart
+++ lib/pages/weekly_emp_timing.dart
@@ -1,14 +1,20 @@
 import 'package:employee_roster_app/custom_components/my_card_component.dart';
+import 'package:employee_roster_app/custom_components/sideline.dart';
+import 'package:employee_roster_app/custom_components/sideview_calender.dart';
 import 'package:flutter/material.dart';
 
-class WeeklyTimings extends StatefulWidget {
-  WeeklyTimings({Key? key}) : super(key: key);
+class WeeklyTimings
+    extends StatefulWidget {
+  WeeklyTimings({Key? key})
+      : super(key: key);
 
   @override
-  _WeeklyTimingsState createState() => _WeeklyTimingsState();
+  _WeeklyTimingsState createState() =>
+      _WeeklyTimingsState();
 }
 
-class _WeeklyTimingsState extends State<WeeklyTimings> {
+class _WeeklyTimingsState
+    extends State<WeeklyTimings> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +24,8 @@ class _WeeklyTimingsState extends State<WeeklyTimings> {
           snap: true,
           floating: true,
           expandedHeight: 140,
-          backgroundColor: Colors.orange,
+          backgroundColor:
+              Colors.orange,
           collapsedHeight: 100,
           title: Text(
             "Wk 24",
@@ -26,14 +33,23 @@ class _WeeklyTimingsState extends State<WeeklyTimings> {
               color: Color(0xff1d1c2c),
               fontSize: 44,
               fontFamily: "Poppins",
-              fontWeight: FontWeight.w500,
+              fontWeight:
+                  FontWeight.w500,
             ),
           ),
         ),
         SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (BuildContext context, int index) {
-              return MyCustomCard();
+          delegate:
+              SliverChildBuilderDelegate(
+            (BuildContext context,
+                int index) {
+              return Row(
+                children: const [
+                  SideviewCalender(),
+                  Sideline(),
+                  MyCustomCard()
+                ],
+              );
             },
             childCount: 7,
           ),
